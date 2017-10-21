80b7ccffad3a58e9e404265707d50dbd72c95a4a
Bryan Berry <bryan@endpoint.com>
2017-10-20 17:39:08 -0700

    form: Added OPT to pass file as message

--- form_dn_submit.py
+++ form_dn_submit.py
@@ -24,6 +24,8 @@ def main():
         default='https://www.democracynow.org/contact/send_contact_email', help="url to submit FORM")
     parser.add_option("-a","--author", dest="author",
         default=False, help="author in \"Name <email>\" format")
+    parser.add_option("-f", "--file", dest="mfile",
+        default=False, help="file containing message to submit FORM")
     parser.add_option("-k", "--subkey", dest="subkey",
         default=52, help=("subject key "
                           "default: 52:Music submissions"))
@@ -47,8 +49,14 @@ def main():
         get_form(options,names)
 
 def get_form(options,names):
-    
-    name = "%s %s" %names[0] names[1]
+
+    # Handle message type
+    if options.mfile:
+        message = get_message(options.mfile)
+    else:
+        message = options.message
+
+    name = "%s %s" %names[0], names[1]
     data = urllib.urlencode({'contact[name]': name },
                             {'contact[email]': names[2] },
                             {'contact[to]': options.subkey },
@@ -60,42 +68,15 @@ def get_form(options,names):
         f.write(response.read())
      
     webbrowser.open("form_dn_results.html") 
- 
-#def get_form_mechanize(url):
-#
-#    # Set browser
-#    br = mechanize.Browser()
-#    br.set_handle_robots(False)
-#    br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
-#
-#    r = br.open(url)
-#    html = r.read()
-#    title = br.title()
-#
-#    # Sample from ipython
-#    #In [24]: br.title()
-#    #u'Contact Us | Democracy Now!'
-#    #In [25]: for f in br.forms():
-#    #    print f
-#    #   ....:     
-#    #<get https://www.democracynow.org/search application/x-www-form-urlencoded
-#    #  <TextControl(query=)>>
-#    #<post https://www.democracynow.org/contact/send_contact_email application/x-www-form-urlencoded
-#    #  <HiddenControl(authenticity_token=i/IhBSNpD7ztyu9mlkBbv5xHfliQK2+bhqMjW/WYnP4=) (readonly)>
-#    #  <TextControl(contact[name]=)>
-#    #  <TextControl(contact[email]=)>
-#    #  <SelectControl(contact[to]=[*, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65])>
-#    #  <TextControl(contact[user_subject]=)>
-#    #  <TextareaControl(contact[message]=)>
-#    #  <TextControl(hobby=)>
-#    #  <SubmitControl(commit=Send message) (readonly)>>
-#    #<post https://www.democracynow.org/subscribe application/x-www-form-urlencoded
-#    #  <HiddenControl(authenticity_token=i/IhBSNpD7ztyu9mlkBbv5xHfliQK2+bhqMjW/WYnP4=) (readonly)>
-#    #  <TextControl(subscriber[email]=)>
-#    #  <TextControl(hobby=)>
-#    #  <SubmitControl(commit=subscribe) (readonly)>>
 
-#    print title
+def get_message(mfile):
+
+    try:
+        with open(mfile,'w') as m:
+            data = m.read()
+            return data
+    except FileNotFoundError:
+        exit(1)
 
 def process_author(author):
 
