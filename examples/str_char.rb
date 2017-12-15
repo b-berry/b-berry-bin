529e1cc50bf470269fe538f7872d8ec27a852d82
Bryan Berry <bryan@endpoint.com>
2017-12-15 11:10:31 -0800

    examples: Ruby string test - brackets

---
+++ examples/str_char.rb
@@ -0,0 +1,27 @@
+#!/usr/bin/ruby
+
+# Test bracket close string
+char_map = {
+    '{' => '}',
+    '[' => ']',
+    '(' => ')',
+    '}' => '{',
+    ']' => '[',
+    ')' => '('
+}
+
+my_str_pass = '{[()]}'
+my_str_fail = '{[(]}'
+
+my_str_pass.split('').each_with_index do |c,i|
+    c_close = char_map.fetch(c)
+    t = my_str_pass.reverse[i]
+    print c,c_close,t
+    if c_close == t
+        puts 'OK'
+        next 
+    else
+        puts 'FAIL'
+        break
+    end
+end 
