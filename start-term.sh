17128be1cc39db47a7c16e3ba7806b22cc905bcc
Bryan Berry <bryan.berry@slalom.com>
2018-08-06 14:19:27 -0700

    start-term: Add OPTS -s -p * -s: tmux session name * -p: 3:project_dir

--- start-term.sh
+++ start-term.sh
@@ -5,72 +5,82 @@ GEOMETRY_X='155x45+35+50'
 PROFILE='default'
 SESS='desktop'
 
+function construct_proj {
+
+  # This will be were project stuff happens
+  return
+
+}
+
+
 function build_tmux {
 
-    STATUS=0
-    T_SESS=$1
+  STATUS=0
+  T_SESS=$1
+  PROJ=$2
 
-    # Set up window names : => dir
-    declare -a conf_tmux=('me' 'far' '2' ':petcoalm-terraform' 'ops' '5')
+  if [ -z $PROJ ]; then
+    PROJ="src"
+  fi
 
-    first=true
-    for n in $(seq 0 $(echo "${#conf_tmux[@]}-1" | bc)); do
-        INSTANCE=${conf_tmux[$n]}
-	    echo $INSTANCE
-	    # Sort item type
-        if [[ $INSTANCE == *":"* ]]; then
-            NAME="${INSTANCE#*:}"
-            DIR=$(find $HOME/* -maxdepth 1 -type d -name $NAME)
-            # Correct dir if not found
-            if [ ! -d $DIR ]; then
-                DIR=$HOME
-            fi
-        else
-            NAME=${conf_tmux[$n]}
-            DIR=$HOME
-        fi
-        # If first create session
-        if $first; then
-            echo "Initiating tmux session: $T_SESS:$NAME"
-            tmux new-sess -d -s $T_SESS -n $NAME
-            T_STATUS=$?
-            if [ $T_STATUS -lt 1 ]; then
-                first=false
-                continue
-            fi
-        fi
-        # Build item in tmux
-        printf "Building tmux new-window $NAME:$DIR "
-        tmux new-window -d -c $DIR -n $NAME -t $T_SESS
-        N_STATUS=$?
-        if [ $N_STATUS -ne 0 ]; then
-            (( STATUS + 1 ))
-            printf "FAIL\n"
-        else
-            printf "OK\n"
-        fi
-    done
+  # Set up window names : => dir
+  declare -a conf_tmux=('me' 'far' '2' ":$PROJ" 'ops' '5')
 
-    report_error $STATUS
+  first=true
+  for n in $(seq 0 $(echo "${#conf_tmux[@]}-1" | bc)); do
+    echo $INSTANCE
+    # Sort item type
+      if [[ $INSTANCE == *":"* ]]; then
+          NAME="${INSTANCE#*:}"
+          DIR=$(find $HOME/* -maxdepth 1 -type d -name $NAME)
+          echo $NAME
+          echo $DIR
+          # Correct dir if not found
+          if [ ! -d $DIR ]; then
+              DIR=$HOME
+          fi
+      else
+          NAME=${conf_tmux[$n]}
+          DIR=$HOME
+      fi
+      # If first create session
+      if $first; then
+          echo "Initiating tmux session: $T_SESS:$NAME"
+          tmux new-sess -d -s $T_SESS -n $NAME
+          T_STATUS=$?
+          if [ $T_STATUS -lt 1 ]; then
+              first=false
+              continue
+          fi
+      fi
+      # Build item in tmux
+      printf "Building tmux new-window $NAME:$DIR "
+      tmux new-window -d -c $DIR -n $(basename $NAME) -t $T_SESS
+      N_STATUS=$?
+      if [ $N_STATUS -ne 0 ]; then
+          (( STATUS + 1 ))
+          printf "FAIL\n"
+      else
+          printf "OK\n"
+      fi
+  done
+
+  report_error $STATUS
 }
 
 function get_env {
 
-    unset E_STATUS
-    printf 'Detecting WM Env: '
-    if [[ ( ! -z $XDG_CURRENT_DESKTOP ) || ( ! -z $GDMSESSION ) ]]; then
-        printf 'OK\n  Desktop: %s\n  Session: %s\n' "$XDG_CURRENT_DESKTOP" "$GDMSESSION"
-        E_STATUS=0
-        WM=$GDMSESSION
-        return 
-    elif [ $TERM_PROGRAM == "Apple_Terminal" ]; then
-        printf 'OK\n  Desktop: %s\n  Session: %s\n' "$TERM_PROGRAM" "$LOGNAME"
-	E_STATUS=0
-	WM=$TERM_PROGRAM
-    else
-        printf 'ERROR\n'
-        E_STATUS=1
-    fi
+  printf 'Detecting WM Env: '
+  if [[ ( ! -z $XDG_CURRENT_DESKTOP ) || ( ! -z $GDMSESSION ) ]]; then
+    printf 'OK\n  Desktop: %s\n  Session: %s\n' "$XDG_CURRENT_DESKTOP" "$GDMSESSION"
+    SET_ENV=$GDMSESSION
+    return
+  elif [ $TERM_PROGRAM == "Apple_Terminal" ]; then
+    printf 'OK\n  Desktop: %s\n  Session: %s\n' "$TERM_PROGRAM" "$LOGNAME"
+    SET_ENV="apple"
+  else
+    printf 'ERROR\n'
+  fi
 
 }
 
@@ -82,11 +92,11 @@ function get_tmux {
     if [ $T_STATUS -gt 0 ]; then
        # Create session
        echo "No tmux:$1 found... initiating"
-       build_tmux $1
+       build_tmux $1 $2
     else
-       echo "Found existing tmux: $1" 
-       return       
-    fi 
+       echo "Found existing tmux: $1"
+       return
+    fi
 
 }
 
@@ -121,53 +131,71 @@ function start_apple_term {
 function usage {
 
     echo "Usage: start_term.sh OPT"
-    echo "       OPT: {-g,--gnome,-x,--xorg}"
+    echo "       OPT: Env:  {-a,--apple,-g,--gnome,-x,--xorg}"
+    echo "       OPT: Tmux: {-s,--sess,-p,--proj}" 
 
 }
 
 
 # Execute
-if [ -z $1 ]; then
-    # Attempt Auto WM detection
-    get_env
-    if [ $E_STATUS -gt 0 ]; then
-        echo "Unable to determine WM Env.  Please specify."
-        usage
-    else
+unset SET_ENV
+while [ "$#" -gt 0 ]; do
+  # Start term in proper env
+  case $1 in
+  -a|--apple)
+      echo "Setting Env: AppleOS"
+      SET_ENV="apple"
+      shift
+      ;;
+  -g|--gnome)
+      echo "Setting Env: Gnome"
+      SET_ENV="gnome"
+      shift
+      ;;
+  -x|--xorg)
+      echo "Setting Env: Xorg"
+      SET_ENV="xorg"
+      shift
+      ;;
+  -p|--proj)
+      echo "Constructing project: $2"
+      PROJ=$2
+      shift
+      shift
+      ;;
+  -s|--sess)
+      echo "Setting session-name: $2"
+      SESS=$2
+      shift
+      shift
+      ;;
+  *)  usage
+      shift
+      ;;
+  esac
+done
 
-        get_tmux $SESS
-
-        case $WM in
-        gnome)
-            start_gnome_term $SESS
-            ;;
-        xorg)
-            start_x_term $SESS
-            ;;
-	Apple_Terminal)
-	    start_apple_term $SESS
-	    ;;
-        esac
-    fi
-else
-    # Start term in proper env
-    case $1 in
-    -a|--apple)
-        echo "Getting tmux"
-        get_tmux $SESS
-        start_apple_term $SESS
-         ;;
-    -g|--gnome)
-        echo "Getting tmux"
-        get_tmux $SESS
-        start_gnome_term $SESS
-         ;;
-    -x|--xorg)
-        echo "Getting tmux"
-        get_tmux $SESS
-        start_x_term $SESS
-        ;;
-    *)  usage
-        ;;
-    esac
+if [ -z $SET_ENV ]; then
+  # Attempt Auto WM detection
+  get_env
+  if [ -z $SET_ENV ]; then
+    echo "Unable to determine WM Env.  Please specify."
+      usage
+  fi
 fi
+
+# Build/Detect Tmux Session
+get_tmux $SESS $PROJ &&\
+
+# Launch terminal in respective env
+case $SET_ENV in
+  gnome)
+    start_gnome_term $SESS
+    ;;
+  xorg)
+    start_x_term $SESS
+    ;;
+  apple)
+    start_apple_term $SESS
+    ;;
+esac
